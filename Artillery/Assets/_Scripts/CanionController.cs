7a050f3b8efb904f599410d1d98d06f6d3667315
Gerardo <gerserrano@outlook.com>
2022-12-04 17:10:45 -0600

    updateInput

--- Artillery/Assets/_Scripts/CanionController.cs
+++ Artillery/Assets/_Scripts/CanionController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class CanionController : MonoBehaviour
 {
@@ -9,13 +11,34 @@ public class CanionController : MonoBehaviour
     [SerializeField] Transform bulletSpawner;
     float rotation;
 
+    public PlayerControls playerControls;
+    InputAction aim, force, shoot;
+
     public static bool blocking;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        aim = playerControls.Canon.Aim;
+        force = playerControls.Canon.ForceShoot;
+        shoot = playerControls.Canon.Shoot;
+
+        aim.Enable();
+        force.Enable();
+        shoot.Enable();
+
+        shoot.performed += Shooting;
+    }
 
     void Update()
     {
-        rotation += Input.GetAxis("Horizontal") * GameManager.instance._turnSpeed;
+        rotation += aim.ReadValue<float>() * GameManager.instance._turnSpeed;
 
-        if(rotation <= 90 && rotation >= 0)
+        if (rotation <= 90 && rotation >= 0)
         {
             transform.eulerAngles = new Vector3(rotation, 90, 0);
         }
@@ -23,26 +46,25 @@ public class CanionController : MonoBehaviour
         if (rotation > 90) rotation = 90;
         if (rotation < 0) rotation = 0;
 
-        if (GameManager.instance._shoots > 0)
-        {
-            if (Input.GetKeyDown(KeyCode.Space) && !blocking)
-            {
-                GameManager.instance._shoots -= 1;
 
-                AudioManager.instance.PlayShoot();
+    }
 
-                GameObject temp = Instantiate(bulletPrefab, bulletSpawner.position, transform.rotation);
-                Rigidbody tempRB = temp.GetComponent<Rigidbody>();
-                CameraFollow.target = temp;
+    private void Shooting(InputAction.CallbackContext context)
+    {
+        GameManager.instance._shoots -= 1;
 
-                Vector3 shootDirection = transform.rotation.eulerAngles;
-                shootDirection.y = 90 - shootDirection.x;
-                Vector3 particlesDirection = new Vector3(-90 + shootDirection.x, 90, 0);
-                GameObject particlesClon = Instantiate(particles, bulletSpawner.position, Quaternion.Euler(particlesDirection), transform);
-                tempRB.velocity = shootDirection.normalized * GameManager.instance._bulletSpeed;
+        AudioManager.instance.PlayShoot();
 
-                blocking = true;
-            }
-        }
+        GameObject temp = Instantiate(bulletPrefab, bulletSpawner.position, transform.rotation);
+        Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+        CameraFollow.target = temp;
+
+        Vector3 shootDirection = transform.rotation.eulerAngles;
+        shootDirection.y = 90 - shootDirection.x;
+        Vector3 particlesDirection = new Vector3(-90 + shootDirection.x, 90, 0);
+        GameObject particlesClon = Instantiate(particles, bulletSpawner.position, Quaternion.Euler(particlesDirection), transform);
+        tempRB.velocity = shootDirection.normalized * GameManager.instance._bulletSpeed;
+
+        blocking = true;
     }
 }
